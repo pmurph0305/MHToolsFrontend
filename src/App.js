75f528827c80961d2eb0835d32796d26c23b8cdf
pmurph0305 <pmurph0305@gmail.com>
2019-02-18 12:02:26 -0500

    Add daily maitenance container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import DailyMaintenance from './Containers/DailyMaintenance/DailyMaintenance'
+import NavBar from './Components/Navigation/NavBar'
 import PHQ9 from './Containers/PHQ9/PHQ9'
 import React, { Component } from 'react';
-import NavBar from './Components/Navigation/NavBar'
 import 'tachyons';
 import './App.css';
 
@@ -8,7 +9,7 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			route: 'phq9'
+			route: 'dm'
 		}
 	}
 
@@ -34,9 +35,11 @@ class App extends Component {
 				? <PHQ9
 					onSubmitPHQ9={this.onSubmitPHQ9}
 					/>
-				: <section className="pa2-ns bt black-90 bg-light-gray">
+				: this.state.route === 'dm'
+				? <DailyMaintenance/>
+				:<section className="pa2-ns bt black-90 bg-light-gray">
 					<h1 className="pa1 ma0">TITLE!</h1>
-					<p>HFUDGSF*DSD</p>
+					<p>Temporary text</p>
 				</section>
 				}
 				{/* <DailyMaintenance/>
---
+++ src/Containers/DailyMaintenance/DailyMaintenance.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+// Displays a list of tasks for a specific day
+// and renders them in a table where each task can be marked as completed or not.
+class DailyMaintenance extends React.Component {
+    render() {
+        return null;
+    }
+}
+
+export default DailyMaintenance;
