1547d1b5e6d0719d20f757a31a1bc256074e3181
pmurph0305 <pmurph0305@gmail.com>
2019-02-18 20:27:53 -0500

    Change Table to row arrays instead of column arrays

--- src/Containers/DailyMaintenance/DailyMaintenance.js
+++ src/Containers/DailyMaintenance/DailyMaintenance.js
@@ -1,10 +1,27 @@
 import React from 'react'
 
+const testTasks = [
+    {
+        taskname: "Wake up at 8 am",
+        completed: false
+    },
+    {
+        taskname: "Eat breakfast",
+        completed: true
+    },
+    {
+        taskname: "Go for a 30 minute walk",
+        completed: false
+    }
+]
+
 
 // Displays a list of tasks for a specific day
 // and renders them in a table where each task can be marked as completed or not.
 class DailyMaintenance extends React.Component {
+    
     render() {
+
         return null
     }
 }
