1438e091d0d75869f4a0b8755a7168ec6918b46e
pmurph0305 <pmurph0305@gmail.com>
2019-04-20 11:39:01 -0400

    Add pending, failed reducers & state

--- src/Containers/History/Redux/history_reducers.js
+++ src/Containers/History/Redux/history_reducers.js
@@ -1,31 +1,58 @@
-import { REQUEST_DM_HISTORY_SUCCESS, REQUEST_PHQ9_HISTORY_SUCCESS } from "./history_constants";
+import {
+	REQUEST_DM_HISTORY_SUCCESS,
+	REQUEST_PHQ9_HISTORY_SUCCESS,
+	REQUEST_DM_HISTORY_PENDING,
+	REQUEST_PHQ9_HISTORY_PENDING,
+    REQUEST_DM_HISTORY_FAILED,
+    REQUEST_PHQ9_HISTORY_FAILED
+} from "./history_constants";
 
-import { updateObject } from '../../../ReduxHelpers/reduxHelpers'
+import { updateObject } from "../../../ReduxHelpers/reduxHelpers";
+
 
 const initialState = {
-    phq9: [],
-    dm: []
+	isPending: false,
+	error: "",
+	phq9: [],
+	dm: []
+};
+
+export function historyReducer(state = initialState, action) {
+	switch (action.type) {
+		// fall through for all pending requests
+		case REQUEST_DM_HISTORY_PENDING:
+		case REQUEST_PHQ9_HISTORY_PENDING:
+            return setRequestPending(state, action);
+        // fall through for all failed requests
+        case REQUEST_DM_HISTORY_FAILED:
+        case REQUEST_PHQ9_HISTORY_FAILED:
+            return setRequestFailedError(state, action);    
+        
+		case REQUEST_DM_HISTORY_SUCCESS:
+			return setDailyMaintenanceHistory(state, action);
+		case REQUEST_PHQ9_HISTORY_SUCCESS:
+			return setPHQ9History(state, action);
+		default:
+			return state;
+	}
 }
 
-export function historyReducer(state = [initialState], action) {
-    switch(action.type) {
-        case REQUEST_DM_HISTORY_SUCCESS:
-            return setDailyMaintenanceHistory(state, action);
-        case REQUEST_PHQ9_HISTORY_SUCCESS:
-            return setPHQ9History(state, action);
-        default:
-            return state;
-    }
+function setRequestPending(state, action) {
+	return updateObject(state, { isPending: true });
+}
+
+function setRequestFailedError(state, action) {
+    return updateObject(state, { isPending: false, error: action.payload })
 }
 
 function setDailyMaintenanceHistory(state, action) {
-    if (Array.isArray(action.payload)) {
-        return updateObject(state, { dm: action.payload })
-    }
+	if (Array.isArray(action.payload)) {
+		return updateObject(state, { dm: action.payload, isPending: false, error: '' });
+	}
 }
 
 function setPHQ9History(state, action) {
-    if (Array.isArray(action.payload)) {
-        return updateObject(state, { phq9: action.payload })
-    }
+	if (Array.isArray(action.payload)) {
+		return updateObject(state, { phq9: action.payload, isPending: false, error: '' });
+	}
 }
