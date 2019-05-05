a018fef341a197ca7f4e1595ec83b736502b2af4
pmurph0305 <pmurph0305@gmail.com>
2019-05-05 12:31:17 -0400

    Request only dm on mount, and others on state change, if empty

--- src/Containers/History/History.js
+++ src/Containers/History/History.js
@@ -5,7 +5,7 @@ import { requestDMHistory, requestPHQ9History } from "./Redux/history_actions";
 
 import "./History.scss";
 import SelectionBox from "../../Components/SelectionBox/SelectionBox";
-import ErrorBox from "../../Components/ErrorBox/ErrorBox"
+import ErrorBox from "../../Components/ErrorBox/ErrorBox";
 const mapStateToProps = state => {
     return {
         user_id: state.appReducer.user_id,
@@ -32,8 +32,9 @@ class History extends React.Component {
     }
 
     componentDidMount() {
-        this.props.onRequestDMHistory(this.props.user_id);
-        this.props.onRequestPHQ9History(this.props.user_id);
+        if (!this.props.dm.length) {
+            this.props.onRequestDMHistory(this.props.user_id);
+        }
     }
 
     dmDataProcess() {
@@ -44,7 +45,6 @@ class History extends React.Component {
                     y: Math.floor((item.completed / item.total) * 1000) / 10
                 };
             });
-            console.log("DM data", data);
             return data;
         }
     }
@@ -57,7 +57,6 @@ class History extends React.Component {
                     y: item.score
                 };
             });
-            console.log("PHQ9 data", data);
             return data;
         }
     }
@@ -66,9 +65,16 @@ class History extends React.Component {
         console.log(event.target.value);
         switch (event.target.value) {
             case "0":
+                if (!this.props.dm.length) {
+                    this.props.onRequestDMHistory(this.props.user_id);
+                }
                 return this.setState({ displayedHistory: "dm" });
             case "1":
+                if (!this.props.phq9.length) {
+                    this.props.onRequestPHQ9History(this.props.user_id);
+                }
                 return this.setState({ displayedHistory: "phq9" });
+
             default:
                 break;
         }
@@ -78,8 +84,8 @@ class History extends React.Component {
         const { error, isPending } = this.props;
         return (
             <div>
-                <div className='historySelectContainer'>
-                    <p className='historySelectContainerText'>Display History For:</p>
+                <div className="historySelectContainer">
+                    <p className="historySelectContainerText">Display History For:</p>
                     <SelectionBox
                         options={["Daily Maintenance", "PHQ9"]}
                         id={"historySelectionBox"}
@@ -87,35 +93,28 @@ class History extends React.Component {
                         onChange={this.onSelectHistoryDisplayed}
                     />
                 </div>
-                <div className='historyChartContainer'>
+                <div className="historyChartContainer">
                     {isPending ? (
                         <p>Loading Data...</p>
-                    )
-                    : error ? (
-                        <ErrorBox error="Error getting history data, please try again"/>
-                    )
-                    : this.state.displayedHistory === "dm" ? (
+                    ) : error ? (
+                        <ErrorBox error="Error getting history data, please try again" />
+                    ) : this.state.displayedHistory === "dm" ? (
                         <ResponsiveContainer width="100%" height="100%">
-                            <LineChart data={this.dmDataProcess()}
-                                margin={{bottom: 20}}
-                            >
-                                <CartesianGrid/>
+                            <LineChart data={this.dmDataProcess()} margin={{ bottom: 20 }}>
+                                <CartesianGrid />
                                 <Line name="Tasks Completed" type="monotone" dataKey="y" stroke="#8884d8" />
-                                <XAxis dataKey="x" label={{ value: "Date", position: "insideBottom", offset: -10}}/>
-                                <YAxis unit="%" label={{ value: "Tasks Completed",angle: -90, offset: 10, position: "insideLeft" }} />
-                                <Tooltip formatter={(value, name, props) => value+'%' }/>
-                               
+                                <XAxis dataKey="x" label={{ value: "Date", position: "insideBottom", offset: -10 }} />
+                                <YAxis unit="%" label={{ value: "Tasks Completed", angle: -90, offset: 10, position: "insideLeft" }} />
+                                <Tooltip formatter={(value, name, props) => value + "%"} />
                             </LineChart>
                         </ResponsiveContainer>
                     ) : this.state.displayedHistory === "phq9" ? (
                         <ResponsiveContainer width="100%" height="100%">
-                            <LineChart data={this.phq9DataProcess()}
-                                margin={{bottom: 20}}
-                            >
-                                <CartesianGrid/>
+                            <LineChart data={this.phq9DataProcess()} margin={{ bottom: 20 }}>
+                                <CartesianGrid />
                                 <Line name="PHQ9 Score" type="monotone" dataKey="y" stroke="#8884d8" />
-                                <XAxis dataKey="x" abel={{ value: "Date", position: "insideBottom", offset: -10}}/>
-                                <YAxis label={{ value: "PHQ9 Score", angle: -90, offset:20, position: "insideLeft" }} />
+                                <XAxis dataKey="x" abel={{ value: "Date", position: "insideBottom", offset: -10 }} />
+                                <YAxis label={{ value: "PHQ9 Score", angle: -90, offset: 20, position: "insideLeft" }} />
                                 <Tooltip />
                                 <Legend />
                             </LineChart>
