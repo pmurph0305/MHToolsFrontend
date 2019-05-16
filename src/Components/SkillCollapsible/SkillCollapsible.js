88f4c7379461992c87023717d0de019c196ae627
pmurph0305 <pmurph0305@gmail.com>
2019-05-16 15:42:02 -0400

    Add some skill collapsible testing

--- src/Components/SkillCollapsible/SkillCollapsible.js
+++ src/Components/SkillCollapsible/SkillCollapsible.js
@@ -1,123 +1,118 @@
 import React from "react";
-import ClickableIcon from '../ClickableIcon/ClickableIcon'
+import ClickableIcon from "../ClickableIcon/ClickableIcon";
 import "./SkillCollapsible.scss";
 
 const Collapsible = ({
-    allowAdd,
-    editing,
-    index,
-    shared,
-    shareable,
-    skill_id,
-    text,
-    title,
-    onAddSharedSkill,
-    onDeleteSkill,
-    onEditSkill,
-    onShareSkill
+  allowAdd,
+  editing,
+  index,
+  shared,
+  shareable,
+  skill_id,
+  text,
+  title,
+  onAddSharedSkill,
+  onDeleteSkill,
+  onEditSkill,
+  onShareSkill
 }) => {
-    function onCollapisbleClick(title, index) {
-        // Get text content element for the skill clicked on.
-        console.log("collapsibleClick");
-        let container = document.getElementById("cDescContainer_" + index);
-        // Collapsible is closing.
-        if (container.style.maxHeight !== "0px" && container.style.maxHeight) {
-            // hide container div border & display.
-            container.style.borderBottom = "0px";
-            container.style.display = "none";
-            container.style.maxHeight = "0px";
-            
-        } else {
-            
-            // collapsible is expanding, so add a border to the container div
-            container.style.borderBottom = "1px solid black";
-            container.style.display = "block";
-            // Set max height to add transition to expanding card.
-            container.style.maxHeight = container.scrollHeight + "px";
-        }
+  function onCollapisbleClick(title, index) {
+    // Get text content element for the skill clicked on.
+    console.log("collapsibleClick");
+    let container = document.getElementById("cDescContainer_" + index);
+    // Collapsible is closing.
+    if (container.style.maxHeight !== "0px" && container.style.maxHeight) {
+      // hide container div border & display.
+      container.style.borderBottom = "0px";
+      container.style.display = "none";
+      container.style.maxHeight = "0px";
+    } else {
+      // collapsible is expanding, so add a border to the container div
+      container.style.borderBottom = "1px solid black";
+      container.style.display = "block";
+      // Set max height to add transition to expanding card.
+      container.style.maxHeight = container.scrollHeight + "px";
     }
+  }
 
-    function onDescriptionChange(area) {
-        if (area) {
-            let container = document.getElementById("cDescContainer_" + index);
-            // set new container maxHeight to be large enough so there's no jiggle when redoing area height.
-            container.style.maxHeight = area.scrollHeight * 2 + "px";
-            area.style.height = 0;
-            area.style.height = area.scrollHeight + "px";
-        }
+  function onDescriptionChange(area) {
+    if (area) {
+      let container = document.getElementById("cDescContainer_" + index);
+      // set new container maxHeight to be large enough so there's no jiggle when redoing area height.
+      container.style.maxHeight = area.scrollHeight * 2 + "px";
+      area.style.height = 0;
+      area.style.height = area.scrollHeight + "px";
     }
+  }
 
-    return (
-        <div>
-            {editing ? (
-                <input
-                    id={"cTitle_" + index}
-                    className="collapsibleTitleInput"
-                    type="text"
-                    defaultValue={title}
-                />
-            ) : (
-                <button
-                    className="collapsible"
-                    id={"cTitle_" + index}
-                    value={title}
-                    onClick={event => onCollapisbleClick(event.target, index)}
-                >
-                    {title}
-                </button>
-            )}
-            <div id={"cDescContainer_" + index} className="collapsibleContent">
-                {editing ? (
-                    <textarea
-                        id={"cDescArea_" + index}
-                        defaultValue={text}
-                        placeholder="Coping Skill Description"
-                        type="text"
-                        aria-describedby="New Task"
-                        className="collapsibleTextArea"
-                        rows="0"
-                        onChange={event => onDescriptionChange(event.target)}
-                    />
-                ) : (
-                    <p className="collapsibleText" id={"cDesc_" + index}>
-                        {text}
-                    </p>
-                )}
-                {shareable && !shared && !editing ? ( // Is a user's created skill, and able to be shared.
-
-                    <ClickableIcon
-                        iconName="share-alt"
-                        onClick={() => onShareSkill(skill_id)}
-                    />
-                ) : null}
-                {!allowAdd && !editing ? ( // Not viewing shared skills, allow editing and deletion.
-                    <>
-                        <ClickableIcon
-                            iconName="close"
-                            onClick={() => onDeleteSkill(index, skill_id)}
-                        />
-                        <ClickableIcon
-                            iconName="create"
-                            onClick={() => onEditSkill(index)}
-                        />
-                    </>
-                ) : editing ? ( 
-                    // Need <> to auto expand container to fit clickable icon.
-                    <> 
-                        <ClickableIcon
-                                iconName="save"
-                                onClick={() => onEditSkill(index)}
-                        />
-                    </>
-                ) : ( // viewing shared skills, can only add skill.
-                    <ClickableIcon
-                        iconName="add-circle-outline"
-                        onClick={() => onAddSharedSkill(skill_id)}
-                    />
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      {editing ? (
+        <input
+          id={"cTitle_" + index}
+          className="collapsibleTitleInput"
+          type="text"
+          defaultValue={title}
+        />
+      ) : (
+        <button
+          className="collapsible"
+          id={"cTitle_" + index}
+          value={title}
+          onClick={event => onCollapisbleClick(event.target, index)}
+        >
+          {title}
+        </button>
+      )}
+      <div id={"cDescContainer_" + index} className="collapsibleContent">
+        {editing ? (
+          <textarea
+            id={"cDescArea_" + index}
+            defaultValue={text}
+            placeholder="Coping Skill Description"
+            type="text"
+            aria-describedby="New Task"
+            className="collapsibleTextArea"
+            rows="0"
+            onChange={event => onDescriptionChange(event.target)}
+          />
+        ) : (
+          <p className="collapsibleText" id={"cDesc_" + index}>
+            {text}
+          </p>
+        )}
+        {shareable && !shared && !editing ? ( // Is a user's created skill, and able to be shared.
+          <ClickableIcon
+            iconName="share-alt"
+            onClick={() => onShareSkill(skill_id)}
+          />
+        ) : null}
+        {!allowAdd && !editing ? ( // Not viewing shared skills, allow editing and deletion.
+          <>
+            <ClickableIcon
+              iconName="close"
+              onClick={() => onDeleteSkill(index, skill_id)}
+            />
+            <ClickableIcon
+              iconName="create"
+              onClick={() => onEditSkill(index)}
+            />
+          </>
+        ) : editing ? (
+          // Need <> to auto expand container to fit clickable icon.
+          <>
+            <ClickableIcon iconName="save" onClick={() => onEditSkill(index)} />
+          </>
+        ) : (
+          // viewing shared skills, can only add skill.
+          <ClickableIcon
+            iconName="add-circle-outline"
+            onClick={() => onAddSharedSkill(skill_id)}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Collapsible;
---
+++ src/Components/SkillCollapsible/SkillCollapsible.test.js
@@ -0,0 +1,92 @@
+import { shallow, mount } from "enzyme";
+import React from "react";
+import SkillCollapsible from "./SkillCollapsible";
+
+describe("skill collapsible tests", () => {
+  let mockAddSkill = jest.fn(val => val);
+  let mockEditSkill = jest.fn(val => val);
+  let mockShareSkill = jest.fn(val => val);
+  let mockDeleteSkill = jest.fn((val, val2) => [val, val2]);
+
+  // not editing their own skill
+  let mockProps = {
+    allowAdd: false,
+    editing: false,
+    index: 1,
+    shared: false,
+    shareable: true,
+    skill_id: 2,
+    text: "test descrption",
+    title: "test title",
+    onDeleteSkill: mockDeleteSkill,
+    onEditSkill: mockEditSkill,
+    onShareSkill: mockShareSkill
+  };
+
+  let wrapper = shallow(<SkillCollapsible {...mockProps} />, {
+    attachTo: document.body
+  });
+  it("displays text and title correctly", () => {
+    expect(wrapper.find(".collapsibleText").text()).toBe(mockProps.text);
+    expect(wrapper.find(".collapsible").text()).toBe(mockProps.title);
+  });
+
+  it("delete, edit, share called", () => {
+    wrapper.find('[iconName="close"]').simulate("click");
+    expect(mockDeleteSkill).toHaveBeenCalledTimes(1);
+    wrapper.find('[iconName="create"]').simulate("click");
+    expect(mockEditSkill).toHaveBeenCalledTimes(1);
+    wrapper.find('[iconName="share-alt"]').simulate("click");
+    expect(mockShareSkill).toHaveBeenCalledTimes(1);
+
+    expect(mockShareSkill.mock.results[0].value).toBe(2);
+    expect(mockEditSkill.mock.results[0].value).toBe(1);
+    expect(mockDeleteSkill.mock.results[0].value).toStrictEqual([1, 2]);
+  });
+
+  // is editing their own skill
+  let mockEditSkill2 = jest.fn(val => val);
+  let mockProps2 = {
+    allowAdd: false,
+    editing: true,
+    index: 6,
+    shared: false,
+    shareable: true,
+    skill_id: 3,
+    text: "test descrption",
+    title: "test title",
+    onDeleteSkill: mockDeleteSkill,
+    onEditSkill: mockEditSkill2,
+    onShareSkill: mockShareSkill
+  };
+  let wrapper2 = shallow(<SkillCollapsible {...mockProps2} />);
+
+  it("calls save correctly", () => {
+    wrapper2.find('[iconName="save"]').simulate("click");
+    expect(mockEditSkill2).toHaveBeenCalledTimes(1);
+    expect(mockEditSkill2.mock.results[0].value).toBe(6);
+  });
+
+  // another users addable skill.
+  let mockProps3 = {
+    allowAdd: true,
+    editing: false,
+    index: 6,
+    shared: false,
+    shareable: true,
+    skill_id: 3,
+    text: "test descrption",
+    title: "test title",
+    onDeleteSkill: mockDeleteSkill,
+    onEditSkill: mockEditSkill2,
+    onShareSkill: mockShareSkill,
+    onAddSharedSkill: mockAddSkill
+  };
+  let wrapper3 = shallow(<SkillCollapsible {...mockProps3} />);
+
+  it("calls add shared skill correctly", () => {
+    wrapper3.find('[iconName="add-circle-outline"]').simulate("click");
+    expect(mockAddSkill).toHaveBeenCalledTimes(1);
+    expect(mockAddSkill.mock.results[0].value).toBe(3);
+  });
+});
