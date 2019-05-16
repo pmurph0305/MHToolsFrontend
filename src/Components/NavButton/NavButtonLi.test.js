7846962e4fc7383bbbc4d7226911fbc1f1f1501c
pmurph0305 <pmurph0305@gmail.com>
2019-05-16 13:28:03 -0400

    Selection box tests

--- src/Components/NavButton/NavButtonLi.test.js
+++ src/Components/NavButton/NavButtonLi.test.js
@@ -1,7 +1,6 @@
 import { shallow, mount } from "enzyme";
 import React from "react";
 import NavButtonLi from "./NavButtonLi";
-import { timeout } from "q";
 
 describe("NavButtonLi tests", () => {
   let wrapper;
---
+++ src/Components/SelectionBox/SelectionBox.test.js
@@ -0,0 +1,44 @@
+import { shallow } from "enzyme";
+import React from "react";
+import SelectionBox from "./SelectionBox";
+
+describe("Selection Box tests", () => {
+  const mockOnChange = jest.fn(val => val);
+  const mockProps = {
+    options: ["a", "b", "c", "d"],
+    id: 1,
+    value: 3,
+    className: "TestClass",
+    label: "TestLabel"
+  };
+  const wrapper = shallow(<SelectionBox {...mockProps} />);
+
+  it("has correct props", () => {
+    expect(wrapper.prop("id")).toBe(mockProps.id);
+    expect(wrapper.prop("value")).toBe(mockProps.value);
+    expect(wrapper.prop("className")).toBe(mockProps.className);
+    expect(wrapper.prop("label")).toBe(mockProps.label);
+    expect(wrapper.prop("aria-label")).toBe(mockProps.label);
+  });
+
+  it("has correct options", () => {
+    expect(
+      wrapper
+        .find("option")
+        .last()
+        .text()
+    ).toBe(mockProps.options[mockProps.options.length - 1]);
+    expect(wrapper.find("option").length).toBe(4);
+    expect(
+      wrapper
+        .find("option")
+        .first()
+        .text()
+    ).toBe(mockProps.options[0]);
+  });
+
+  const wrapper2 = shallow(<SelectionBox />);
+  it("correctly sets class to default", () => {
+    expect(wrapper2.prop("className")).toBe("DefaultSelect");
+  });
+});
