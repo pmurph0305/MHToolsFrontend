b47d4caf998bbed7bd8fd48a99dfd8d4f132b583
pmurph0305 <pmurph0305@gmail.com>
2019-06-04 14:20:47 -0400

    Modify TaskItem component & css to use flexbox.

--- src/Containers/DailyMaintenance/TaskItem/TaskItem.js
+++ src/Containers/DailyMaintenance/TaskItem/TaskItem.js
@@ -15,92 +15,88 @@ import "./TaskItem.scss";
  * @param  {function} onEditClick EventHandler - the edit/save task icon is clicked
  */
 const TaskItem = ({
-	task,
-	id,
-	onCheck,
-	onRemove,
-	onChange,
-	onRankChange,
-	onEditClick,
-	checked,
-	editing,
-	allowEditing
+  task,
+  id,
+  onCheck,
+  onRemove,
+  onChange,
+  onRankChange,
+  onEditClick,
+  checked,
+  editing,
+  allowEditing
 }) => {
-	return (
-		<div className="taskContainer">
-			<div>
-                {/* Rank Change Arrows */}
-				<ul className="arrowContainer">
-					<li>
-						<ClickableIcon
-							iconName="arrow-up"
-							iconSize="small"
-							onClick={() => onRankChange(id, -1)}
-						/>
-					</li>
-					<li>
-						<ClickableIcon
-							iconName="arrow-down"
-							iconSize="small"
-							onClick={() => onRankChange(id, 1)}
-						/>
-					</li>
-				</ul>
+  return (
+    <div className="taskContainer">
+      {/* Rank Change Arrows */}
+      <ul className="arrowContainer">
+        <li>
+          <ClickableIcon
+            iconName="arrow-up"
+            iconSize="small"
+            onClick={() => onRankChange(id, -1)}
+          />
+        </li>
+        <li>
+          <ClickableIcon
+            iconName="arrow-down"
+            iconSize="small"
+            onClick={() => onRankChange(id, 1)}
+          />
+        </li>
+      </ul>
 
-                {/* Edit / Delete Icons */}
-				{allowEditing ? (
-					<div className="iconContainer">
-						{editing ? (
-							<ClickableIcon
-								iconName="save"
-								iconDesc="Save task"
-								onClick={() => onEditClick(id)}
-							/>
-						) : (
-							<ClickableIcon
-								iconName="create"
-								iconDesc="Edit task"
-								onClick={() => onEditClick(id)}
-							/>
-						)}
-						<ClickableIcon
-							iconName="close"
-							iconDesc="Delete task"
-							onClick={() => onRemove(id)}
-						/>
-					</div>
-				) : null}
-                
-                {/* Task text display and input field when editing */}
-				<label htmlFor={id}>
-					{editing ? (
-						<input
-							id={"task_" + id}
-							className="taskInput"
-							value={task}
-							label={"Edit task " + id + " text field"}
-							aria-label={"Edit task " + id + " text field"}
-							onChange={(event) =>
-								onChange(id, event.target.value)
-							}
-						/>
-					) : (
-						<div className="taskText">{task}</div>
-					)}
-                    
-                    {/* Custom checkbox */}
-					<input
-						type="checkbox"
-						className="hiddenCheckbox"
-						id={id}
-						onClick={onCheck}
-						defaultChecked={checked}
-					/>
-					<span className="checkmark" />
-				</label>
-			</div>
-		</div>
-	);
+      {/* Custom checkbox */}
+      <div className="dm-checkbox-container">
+        <input
+          type="checkbox"
+          className="styled-checkbox"
+          id={id}
+          onClick={onCheck}
+          defaultChecked={checked}
+        />
+        <label htmlFor={id} />
+      </div>
+
+      {/* Task text display and input field when editing */}
+      {editing ? (
+        <input
+          id={"task_" + id}
+          className="taskInput"
+          value={task}
+          label={"Edit task " + id + " text field"}
+          aria-label={"Edit task " + id + " text field"}
+          onChange={event => onChange(id, event.target.value)}
+        />
+      ) : (
+        <div className="taskText">{task}</div>
+      )}
+
+      {/* Edit / Delete Icons */}
+      {allowEditing && (
+        <div className="iconContainer">
+          {editing ? (
+            <ClickableIcon
+              iconName="save"
+              iconDesc="Save task"
+              onClick={() => onEditClick(id)}
+            />
+          ) : (
+            <ClickableIcon
+              iconName="create"
+              iconDesc="Edit task"
+              onClick={() => onEditClick(id)}
+            />
+          )}
+          <ClickableIcon
+            iconName="close"
+            iconDesc="Delete task"
+            onClick={() => onRemove(id)}
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default TaskItem;
