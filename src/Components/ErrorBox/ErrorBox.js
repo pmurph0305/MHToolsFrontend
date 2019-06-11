c70ed6fb0e5a76041b1554b1b977a02107653a9f
pmurph0305 <pmurph0305@gmail.com>
2019-06-11 13:37:28 -0400

    Errobox docs & proptypes

--- src/Components/ErrorBox/ErrorBox.js
+++ src/Components/ErrorBox/ErrorBox.js
@@ -1,35 +1,35 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-import './ErrorBox.css'
+import "./ErrorBox.css";
 /**
+ * ErrorBox - Returns a component that displays the error. Splits the given error on 'error:'
  * @param  {string} {error} Error to display from fetch
  */
-const ErrorBox = ({error}) => {
-    var displayedError;
-    
-    if (error && error.length) {
-        
-        // convert error to string to split.
-        error = error.toString();
-        // Removes error passed from database.
-        displayedError = error.split('error:')
-        // Still console log the actual database error in console though.
-        if (displayedError[1]) {
-            console.log(displayedError[1]);
-        }
-        displayedError = displayedError[0].trim();
+const ErrorBox = ({ error }) => {
+  var displayedError;
+
+  if (error && error.length) {
+    // convert error to string to split.
+    error = error.toString();
+    // Removes error passed from database.
+    displayedError = error.split("error:");
+    // Still console log the actual database error in console though.
+    if (displayedError[1]) {
+      console.log(displayedError[1]);
     }
+    displayedError = displayedError[0].trim();
+  }
 
-    return (
-        <div>
-            {displayedError
-            ? <div className='ErrorBox'>
-                {displayedError}
-              </div>
-            : null
-            }
-        </div>
-    )
-}
+  return (
+    <div>
+      {displayedError ? <div className="ErrorBox">{displayedError}</div> : null}
+    </div>
+  );
+};
 
-export default ErrorBox
+ErrorBox.propTypes = {
+  error: PropTypes.string.isRequired
+};
+
+export default ErrorBox;
