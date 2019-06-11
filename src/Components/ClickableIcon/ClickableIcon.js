fe88e8c95e94260a6a97b4d980099d22ef874357
pmurph0305 <pmurph0305@gmail.com>
2019-06-11 13:16:43 -0400

    Clickable Icon default & propTypes

--- src/Components/ClickableIcon/ClickableIcon.js
+++ src/Components/ClickableIcon/ClickableIcon.js
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React from "react";
+import PropTypes from "prop-types";
 
 /**
  * @param  {string} iconName ionicon icon name
@@ -7,13 +7,16 @@ import React from 'react'
  * @param  {function} onClick onClick event handler
  */
 const ClickableIcon = ({ iconName, iconSize, onClick }) => {
-    return (
-        <ion-icon
-            name={iconName}
-            onClick={onClick}
-            size={iconSize ? iconSize : "default"}            
-        />
-    )
-}
+  return <ion-icon name={iconName} onClick={onClick} size={iconSize} />;
+};
+
+ClickableIcon.defaultProps = {
+  iconSize: "default"
+};
+
+ClickableIcon.propTypes = {
+  iconName: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+};
 
 export default ClickableIcon;
