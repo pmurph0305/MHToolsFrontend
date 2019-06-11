dc1e0107a0841f3455368712164f2d63072c92f9
pmurph0305 <pmurph0305@gmail.com>
2019-06-11 14:20:55 -0400

    NavButtonLi propTypes & docs

--- src/Components/ModalForms/SignInForm.js
+++ src/Components/ModalForms/SignInForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./SignInForm.scss";
 
 /** SignInForm form - returns a component that renders a signin form
--- src/Components/NavButton/NavButtonLi.js
+++ src/Components/NavButton/NavButtonLi.js
@@ -1,18 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 /**
+ * NavButtonLi - returns a li element that contains a Link element to the route, which contains a button with the button label.
  * @param {string} buttonLabel text displayed on the button
- * @param {string} liClasss className for each button li element default: null
+ * @param {string} liClasss className for each button li element
  * @param {string} route Route for react router
  */
 const NavButtonLi = ({ buttonLabel, route, liClass }) => {
   return (
-    <li className={liClass ? liClass : null}>
+    <li className={liClass}>
       <Link to={`/${route}`}>
         <button>{buttonLabel}</button>
       </Link>
     </li>
   );
+};
+
+NavButtonLi.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+  route: PropTypes.string.isRequired,
+  liClass: PropTypes.string
 };
 
 export default NavButtonLi;
