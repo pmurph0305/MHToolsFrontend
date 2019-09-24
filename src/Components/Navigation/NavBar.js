0aa1d33d9548ec2728561585cab707dbd6b4ccaa
unknown <pmurph0305@gmail.com>
2019-09-24 16:07:32 -0400

    Switch to a hamburger menu

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -1,7 +1,7 @@
 import logo from "./mh_logo_white.png";
 import NavButtonLi from "../NavButton/NavButtonLi";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 
 import "./NavBar.scss";
@@ -10,40 +10,102 @@ import "./NavBar.scss";
  * @callback onModalChange called passing a string of the modal to open.
  */
 const NavBar = props => {
+  const menuEl = useRef(null);
+
+  let onClickMenuIcon = () => {
+    if (menuEl.current) {
+      if (menuEl.current.getAttribute("class") === "nav__menu") {
+        menuEl.current.setAttribute("class", "nav__menu open-menu");
+      } else {
+        menuEl.current.setAttribute("class", "nav__menu");
+      }
+    }
+  };
+
+  let onNavClick = () => {
+    if (menuEl.current) {
+      if (menuEl.current.getAttribute("class") === "nav__menu open-menu") {
+        menuEl.current.setAttribute("class", "nav__menu");
+      }
+    }
+  };
+
+  let onUserModalsClick = entry => {
+    onNavClick();
+    props.onModalChange(entry);
+  };
+
   return (
-    <header className="navHeader">
-      <nav className="navClass" role="navigation">
-        <ul>
-          <li>
-            <Link to="/">
-              <img className="" alt="home" src={logo} />
+    <header className="nav__header">
+      <nav className="nav__main" role="navigation">
+        <ul ref={menuEl} className="nav__menu">
+          <li onClick={onNavClick} className="nav__item">
+            <Link className="nav__link nav__link--image" to="/">
+              <img className="nav__image" alt="home" src={logo} />
             </Link>
           </li>
-          <NavButtonLi buttonLabel="Daily Maintenance" route="dm" />
-          <NavButtonLi buttonLabel="PHQ-9" route="phq9" />
-          <NavButtonLi buttonLabel="CBT" route="cbt" />
-          <NavButtonLi buttonLabel="Coping Skills" route="coping" />
-          <NavButtonLi buttonLabel="History" route="hist" />
+          <li onClick={onNavClick} className="nav__item">
+            <Link className="nav__link" to="/dm">
+              Daily Maintenance
+            </Link>
+          </li>
+          <li onClick={onNavClick} className="nav__item">
+            <Link className="nav__link" to="/phq9">
+              PHQ-9
+            </Link>
+          </li>
+          <li onClick={onNavClick} className="nav__item">
+            <Link className="nav__link" to="/cbt">
+              CBT
+            </Link>
+          </li>
+          <li onClick={onNavClick} className="nav__item">
+            <Link className="nav__link" to="/coping">
+              Coping Skills
+            </Link>
+          </li>
+          <li onClick={onNavClick} className="nav__item">
+            <Link className="nav__link" to="/hist">
+              History
+            </Link>
+          </li>
           {!props.isSignedIn ? (
             <>
-              <li className="sign-ins">
-                <button onClick={() => props.onModalChange("register")}>
+              <li className="nav__item nav__item--right">
+                <button
+                  className="nav__button"
+                  onClick={() => onUserModalsClick("register")}
+                >
                   Register
                 </button>
               </li>
-              <li className="sign-ins">
-                <button onClick={() => props.onModalChange("signin")}>
+              <li className="nav__item">
+                <button
+                  className="nav__button"
+                  onClick={() => onUserModalsClick("signin")}
+                >
                   Sign in
                 </button>
               </li>
             </>
           ) : (
-            <li className="sign-ins">
-              <button onClick={() => props.onModalChange("signout")}>
+            <li className="nav__item nav__item--right">
+              <button
+                className="nav__button"
+                onClick={() => onUserModalsClick("signout")}
+              >
                 Sign out
               </button>
             </li>
           )}
+          <li className="nav__item nav__item--icon">
+            <button
+              className="nav__button nav__button--icon"
+              onClick={onClickMenuIcon}
+            >
+              <ion-icon class="nav__icon" name="menu" size="large" />
+            </button>
+          </li>
         </ul>
       </nav>
     </header>
